1bffd4de3ba39b6f4f46b2a12018692eeda3a4a3
s-zhouj <s-zhouj@54L1809-26.it.bsd405.org>
2014-12-05 08:20:20 -0800

    init commit

---
+++ PokemonRevolution/src/Pokemon.java
@@ -0,0 +1,108 @@
+
+public class Pokemon {
+
+    private String name;
+    private int speed;
+    private int defense;
+    private int hp;
+    private Attack[] attacks;
+    private int damage;
+    private int energy = 0;
+
+    public Pokemon(String name, int speed, int defense, int hp, Attack[] attacks) {
+        //invariant testing
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Name must contain something!");
+        }
+        if (speed <= 0 || defense <= 0 || hp <= 0) {
+            throw new IllegalArgumentException("Speed, defense, and HP must be positive!");
+        }
+        if (attacks.length == 0) {
+            throw new IllegalArgumentException("You have to have at least one attack!");
+        }
+        if (speed + defense + hp > 150) {
+            throw new IllegalArgumentException("Speed, defense, and HP combined can't exceed 150!");
+        }
+        if (attacks.length > 3) {
+            throw new IllegalArgumentException("You can't have more than 3 attacks!");
+        }
+        int attackvalue = 0;
+        int attackenergy = 0;
+        for (int i = 0; i < attacks.length; i++) {
+            attackvalue += Math.abs(attacks[i].getValue());
+            attackenergy += attacks[i].getEnergy();
+        }
+        if (attackvalue > 150) {
+            throw new IllegalArgumentException("Total absolute value of its attacks cannot exceed 150!");
+        }
+        if (attackenergy < attackvalue / 3) {
+            throw new IllegalArgumentException("Total attack energy must be more than 1/3 of the total absolute value!");
+        }
+        this.name = name;
+        this.speed = speed;
+        this.defense = defense;
+        this.hp = hp;
+        this.attacks = attacks;
+    }
+
+    public Pokemon(String name, Attack[] attacks) {
+        this(name, 50, 50, 50, attacks);
+    }
+    
+    public void setDamage(int damage) {
+        if (damage <= 0) {
+            throw new IllegalArgumentException("Damage must be greater than 0!");
+        }
+        this.damage = damage;
+    }
+    
+    public void setEnergy(int energy) {
+        if (energy <= 0) {
+            throw new IllegalArgumentException("Energy must be greater than 0!");
+        }
+        this.energy = energy;
+    }
+
+    public boolean isWeak(Pokemon pokemon) {
+        return false;
+    }
+
+    public boolean isResistant(Pokemon pokemon) {
+        return false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public Attack[] getAttacks() {
+        return this.attacks;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public int getEnergy() {
+        return this.energy;
+    }
+
+    public String toString() {
+        String attackinfo = "";
+        for (int i = 0; i < attacks.length; i++) {
+            //todo stuff
+        }
+    }
+}
