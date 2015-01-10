3408044f98628b0904609ab863e6af706f065e9d
s-zhouj <s-zhouj@54L1808-08.it.bsd405.org>
2015-01-09 16:32:04 -0800

    done???

--- Student/src/Student.java
+++ Student/src/Student.java
@@ -58,11 +58,51 @@ public class Student implements Comparable<Student> {
     }
 
     public int compareTo(Student o) {
-        //TODO
-        return 0;
+        //test school
+        int i = getSchoolName().compareTo(o.getSchoolName());
+        if (i != 0) {
+            return i;
+        }
+        //test grade
+        i = getGrade() - o.getGrade();
+        if (i != 0) {
+            return i;
+        }
+        //test last name
+        i = getLastName().compareTo(o.getLastName());
+        if (i != 0) {
+            return i;
+        }
+        //test first name
+        i = getFirstName().compareTo(o.getFirstName());
+        if (i != 0) {
+            return i;
+        }
+        //test student id
+        return getID() - o.getID();
     }
 
     public static void main(String[] args) {
         ArrayList<Student> list = new ArrayList<Student>();
+        list.add(new Student("Generic High School", 12, "John", "Smiththisnameisafakename", 1234567));
+        list.add(new Student("Ridgewing Private", 10, "Bobby", "Robert'); DROP TABLE students;--", 8920371));
+        list.add(new Student("Video Game High School", 8, "Daniel", "Li", 4986257));
+        list.add(new Student("Ridgewing Private", 5, "Elaine", "Robert", 1467355));
+        list.add(new Student("Video Game High School", 8, "Daniel", "Li", 1643589));
+        list.add(new Student("Generic High School", 6, "Helpimtrappedin", "anamegeneratingprogram", 4986357));
+        list.add(new Student("This Is Not A High School", 1, "Amara", "Liu", 431613));
+        list.add(new Student("Derpy Middle School", 7, "Sophie", "Altira", 49883));
+        list.add(new Student("Derpy Middle School", 7, "Kenny", "Altira", 49884));
+        list.add(new Student("Derpy Middle School", 7, "Benny", "Altira", 49895));
+        list.add(new Student("Generic Middle School", 9, "Leslie", "Burke", 66576));
+        String print = "";
+        for (Student student : list) {
+            print += student.getFirstName() + " "
+                    + student.getLastName() + " "
+                    + "(ID: " + student.getID() + "): "
+                    + student.getSchoolName() + " "
+                    + "grade " + student.getGrade() + "\n";
+        }
+        System.out.print(print);
     }
 }
