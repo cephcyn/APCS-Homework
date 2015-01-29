7f2b8b9d7f4e3052a5bb5a999bb86d0d962ad151
s-zhouj <s-zhouj@54L1809-27.it.bsd405.org>
2015-01-29 08:49:32 -0800

    Done!

--- BubbleSort/src/BubbleSort.java
+++ BubbleSort/src/BubbleSort.java
@@ -12,8 +12,8 @@ public class BubbleSort {
 
     public static int bubbleSort(int[] input) {
         int numswaps = 0;
-        for(int i = input.length - 1; i >= 0; i++) {
-            for(int j = 0; j < i; j++) {
+        for (int i = input.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (input[j] > input[j + 1]) {
                     swap(input, j);
                     numswaps++;
@@ -22,23 +22,23 @@ public class BubbleSort {
         }
         return numswaps;
     }
-    
+
     private static void swap(int[] input, int index) {
         int temp = input[index];
         input[index] = input[index + 1];
         input[index + 1] = temp;
     }
-    
+
     public static void main(String[] args) {
         int[] list;
-        for(int i = 1; i <= 1000; i *= 10) {
+        for (int i = 1; i <= 1000; i *= 10) {
             list = new int[i];
             initializeSort(list);
             int swapcount = bubbleSort(list);
             //print the list
             System.out.print("[" + list[0]);
-            for(int val : list) {
-                System.out.print(", " + val);
+            for (int j = 1; j < list.length; j++) {
+                System.out.print(", " + list[j]);
             }
             System.out.println("]");
             //print the number of counts
