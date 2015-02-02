7feda0c93e386738d3556dbaab787acb7fa9ab63
s-zhouj <s-zhouj@54L1809-26.it.bsd405.org>
2015-02-02 08:19:53 -0800

    just merge() left to do

--- BubbleSort/src/MergeSort.java
+++ BubbleSort/src/MergeSort.java
@@ -1,15 +1,31 @@
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     public static void main(String[] args) {
-
     }
 
     void sort(int[] a) {
+        if (a.length > 1) {
+            //split into left and right
+            int[] left = Arrays.copyOfRange(a, 0, a.length / 2);
+            int[] right = Arrays.copyOfRange(a, a.length / 2, a.length);
 
+            //sort each subarray
+            sort(left);
+            sort(right);
+
+            //merge the two subarrays
+            merge(a, left, right);
+        }
     }
 
     void merge(int a[], int[] left, int[] right) {
+        int il = 0;
+        int ir = 0;
 
+        for (int i = 0; i < a.length; i++) {
+        }
     }
 }
