2205bedaff91050eb98a2da6d3a2856f09674f7f
Winterstorm <Winterstorm@M400>
2015-02-03 16:55:58 -0800

    mergesort complete

--- SortAnalysis/src/MergeSort.java
+++ SortAnalysis/src/MergeSort.java
@@ -3,35 +3,44 @@ import java.util.Arrays;
 
 public class MergeSort implements ISorter {
 
+    int numComparisons;
+    int numAssignments;
+
     public MergeSort() {
+        this.numComparisons = 0;
+        this.numAssignments = 0;
     }
 
     public SortStats sort(int[] a) {
-        //init sortstat variables
-        int numComparisons = 0;
-        int numAssignments = 0;
+        //init sortstat variables (this is separate so that variable tracking works)
+        this.numComparisons = 0;
+        this.numAssignments = 0;
         long startTime = System.nanoTime();
-        //actual sort alg
+        //call the actual sort alg
+        splitsort(a);
+        return new SortStats("Merge Sort",
+                a.length,
+                this.numComparisons,
+                this.numAssignments,
+                System.nanoTime() - startTime);
+    }
+
+    private void splitsort(int[] a) {
         if (a.length > 1) {
             //split into left and right
             int[] left = Arrays.copyOfRange(a, 0, a.length / 2);
             int[] right = Arrays.copyOfRange(a, a.length / 2, a.length);
 
             //sort each subarray
-            sort(left);
-            sort(right);
+            splitsort(left);
+            splitsort(right);
 
             //merge the two subarrays
             merge(a, left, right);
         }
-        return new SortStats("Merge Sort",
-                a.length,
-                numComparisons,
-                numAssignments,
-                System.nanoTime() - startTime);
     }
 
-    private static void merge(int a[], int[] left, int[] right) {
+    private void merge(int a[], int[] left, int[] right) {
         int il = 0;
         int ir = 0;
 
@@ -40,18 +49,24 @@ public class MergeSort implements ISorter {
             if (il >= left.length) {
                 a[i] = right[ir];
                 ir++;
+                numAssignments++;
                 //if the right array is used up
             } else if (ir >= right.length) {
                 a[i] = left[il];
                 il++;
+                numAssignments++;
                 //if the left header is smaller
             } else if (left[il] <= right[ir]) {
                 a[i] = left[il];
                 il++;
+                numComparisons++;
+                numAssignments++;
                 //if the right header is smaller
             } else {
                 a[i] = right[ir];
                 ir++;
+                numComparisons++;
+                numAssignments++;
             }
         }
     }
