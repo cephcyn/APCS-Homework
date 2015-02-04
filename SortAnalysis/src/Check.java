bda6a4d6502e63ab25a31fe4b265bf03e2bcc11e
Winterstorm <Winterstorm@M400>
2015-02-03 17:28:00 -0800

    check class done

--- SortAnalysis/src/Check.java
+++ SortAnalysis/src/Check.java
@@ -1,4 +1,11 @@
 
 public class Check {
-
+    public static boolean isInOrder(int[] a) {
+        for(int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- SortAnalysis/src/Program.java
+++ SortAnalysis/src/Program.java
@@ -1,4 +1,41 @@
 
+import java.util.Random;
+
 public class Program {
 
+    public static void main(String[] args) {
+        int[] list;
+
+        //set one (random init, varying length)
+        for (int i = 1; i <= 4096; i *= 2) {
+            list = new int[i];
+            randomInit(list);
+        }
+
+        //set two (custom init, fixed length)
+    }
+
+    private static void randomInit(int[] a) {
+        Random rand = new Random();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rand.nextInt();
+        }
+    }
+    
+    private static void reverseInit(int[] a) {
+        
+    }
+    
+    private static void orderInit(int[] a) {
+        
+    }
+    
+    private static String formatArray(int[] a) {
+        String out = "[" + a[0];
+        for(int i = 1; i < a.length; i++) {
+            out += ", " + a[i];
+        }
+        out += "]";
+        return out;
+    }
 }
