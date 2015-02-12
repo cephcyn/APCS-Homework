e7728a11f72d71bf5cc18820a47fd21a9c0f78e8
s-zhouj <s-zhouj@54L1809-27.it.bsd405.org>
2015-02-12 08:06:00 -0800

    oldsearch added

--- RecursiveBinarySearch/src/RecursiveBinarySearch.java
+++ RecursiveBinarySearch/src/RecursiveBinarySearch.java
@@ -9,4 +9,18 @@ public class RecursiveBinarySearch {
     public int binarySearch(int[] a, int target) {
         
     }
+    public int oldBinarySearch(int[] a, int target) {
+        int min = 0, max = a.length - 1;
+        while(min <= max) {
+            int index = (max + min) / 2;
+            if (target == a[index]) {
+                return index;
+            } else if (target > a[index]) {
+                min = index + 1;
+            } else {
+                max = index - 1;
+            }
+        }
+        return -1;
+    }
 }
