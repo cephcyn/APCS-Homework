d6146bb699d460c20d53db4061a9f121ab5cae46
s-zhouj <s-zhouj@54L1809-27.it.bsd405.org>
2015-02-12 08:40:57 -0800

    done!

--- RecursiveBinarySearch/src/RecursiveBinarySearch.java
+++ RecursiveBinarySearch/src/RecursiveBinarySearch.java
@@ -6,8 +6,15 @@
 public class RecursiveBinarySearch {
 
     public static void main(String[] args) {
-        int[] a = {0, 1, 2, 3, 4, 5};
-        System.out.println(binarySearch(a, 2));
+        int[] a = {0, 1, 2, 3, 4, 5, 9};
+        int[] b = {-4, 2, 5, 19, 50, 61};
+        int[] c = {-15, -10, -5, 0, 5};
+        System.out.println(binarySearch(a, 9));
+        System.out.println(binarySearch(b, -4));
+        System.out.println(binarySearch(c, 0));
+        System.out.println(binarySearch(a, 3));
+        System.out.println(binarySearch(b, 70));
+        System.out.println(binarySearch(c, 300));
     }
 
     public static int binarySearch(int[] a, int target) {
@@ -16,13 +23,13 @@ public class RecursiveBinarySearch {
 
     private static int recursivePortion(int[] a, int target, int min, int max) {
         int mid = (min + max) / 2;
-        if (a.length != 1) {
+        if (min < max) {
             if (a[mid] == target) {
                 return mid;
-            } else if (a[mid] < target) {
+            } else if (a[mid] > target) {
                 return recursivePortion(a, target, min, mid);
             } else {
-                return recursivePortion(a, target, mid, max);
+                return recursivePortion(a, target, mid + 1, max);
             }
         }
         return -1;
