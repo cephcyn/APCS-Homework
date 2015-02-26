13a2eeda44853dd59b779419b736cebaf1a72ae8
s-zhouj <s-zhouj@54L1809-27.it.bsd405.org>
2015-02-26 08:49:15 -0800

    stuff done

--- Boggle/src/Board.java
+++ Boggle/src/Board.java
@@ -21,14 +21,43 @@ public class Board {
             }
         }
         Random rand = new Random();
-        //randomly put in a prefix
-        String[] prefixes = {"RE-", "STR-", "UN-"};
-        letters[rand.nextInt(letters.length)][rand.nextInt(letters.length)] = prefixes[rand.nextInt(prefixes.length)];
+        //randomly put in a prefix (50% chance)
+        if (rand.nextDouble() >= 0.5) {
+            String[] prefixes = {"RE-", "STR-", "UN-"};
+            letters[rand.nextInt(letters.length)][rand.nextInt(letters.length)] = prefixes[rand.nextInt(prefixes.length)];
+        }
     }
 
     public ArrayList<String> find() {
+        return new ArrayList<String>(5);
     }
 
     public String toString() {
+        String output = "";
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < letters.length; j++) {
+                output += spaceLetters(letters[i][j]);
+            }
+            output += "\n";
+        }
+        return output;
+    }
+
+    private String spaceLetters(String letter) {
+        String output = letter;
+        //find the longest letter and space it such (accounting for prefixes)
+        int longest = 0;
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < letters.length; j++) {
+                if (letters[i][j].length() > longest) {
+                    longest = letters[i][j].length();
+                }
+            }
+        }
+        //space the letter
+        while (output.length() < longest) {
+            output += " ";
+        }
+        return output;
     }
 }
--- Boggle/src/Program.java
+++ Boggle/src/Program.java
@@ -7,7 +7,9 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Boggle, by Joyce Zhou.");
-        WordList wordlist = new WordList("WordList.txt", 3, 8);
-        //TODO
+        WordList wordlist = new WordList("WordList.txt", 3, Integer.MAX_VALUE);
+        Board board = new Board(wordlist, 4);
+        System.out.println(board);
+        //TODO print find()
     }
 }
