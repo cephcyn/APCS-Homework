fb2f4e44b72c808a3fe73aa8052db1a82a067e52
s-zhouj <s-zhouj@54L1809-26.it.bsd405.org>
2015-02-27 08:56:07 -0800

    ... done?

--- Boggle/src/Program.java
+++ Boggle/src/Program.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Joyce Zhou
@@ -10,6 +12,21 @@ public class Program {
         WordList wordlist = new WordList("WordList.txt", 3, Integer.MAX_VALUE);
         Board board = new Board(wordlist, 4);
         System.out.println(board);
-        System.out.println(board.find());
+        System.out.println(formatWordList(board.find()));
+    }
+
+    public static String formatWordList(ArrayList<String> words) {
+        String output = "Found " + words.size() + " word(s). \n";
+        if (words.size() > 0) {
+            int currentWordLength = 0;
+            for (int i = 0; i < words.size(); i++) {
+                if (words.get(i).length() != currentWordLength) {
+                    currentWordLength = words.get(i).length();
+                    output += "\n" + currentWordLength + " letter words \n";
+                }
+                output += words.get(i) + "\n";
+            }
+        }
+        return output;
     }
 }
